e31c7386863a661bedfb3d22a2105eb6ed6e2335
Austin Henley <azh321@gmail.com>
2021-04-18 11:53:37 -0400

    Refactored into a class.

--- wiggleface.py
+++ wiggleface.py
@@ -2,6 +2,49 @@
 import pygame
 import math
 
+class wiggleface:
+    def __init__(self):
+        pygame.init()
+        self.surface = pygame.display.set_mode((800,600))
+        self.time = 1000
+        # Color palette: https://lospec.com/palette-list/endesga-8
+        self.colors = [(253,253,248), (211,39,52), (218,125,34), (230,218,41), (40,198,65), (45,147,221), (123,83,173), (27,28,51)]
+        self.loop()
+
+    def loop(self):
+        # Run until the user asks to quit
+        done = False
+        while not done:
+            # Did the user click the window close button?
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+
+            # Fill the background with white
+            self.surface.fill(self.colors[-1])
+
+            columns = 16
+            rows = 16
+            xOffset = 32
+            yOffset = 32
+            xGap = 8
+            yGap = 8
+            width = 16
+            height = 16
+            scaling = math.sin(self.time/30)*3
+            for j in range(columns):
+                for k in range(rows):
+                    tX = xOffset + j * (width + xGap)
+                    tY = yOffset + k * (height + yGap)
+                    c = (j+k+(self.time//30))%8
+                    rectrot(self.surface, self.colors[c], pygame.Rect((tX, tY), (width-scaling, height-scaling)), 0, 1, self.time/3)
+
+            pygame.display.flip()
+            self.time += 1
+
+        # Done! Time to quit.
+        pygame.quit()
+
 # https://stackoverflow.com/a/66688131/938695
 def rectrot( surface, color, pos, fill, border_radius, angle ):
     """
@@ -14,44 +57,7 @@ def rectrot( surface, color, pos, fill, border_radius, angle ):
     pygame.draw.rect(s, color,(0,0,pos[2],pos[3]),fill, border_radius=border_radius)
     s = pygame.transform.rotate(s,angle)
     surface.blit( s, (pos[0],pos[1]) )
-  
-pygame.init()
-surface = pygame.display.set_mode((800,600))
-  
-time = 0
-# Color palette: https://lospec.com/palette-list/endesga-8
-colors = [(253,253,248), (211,39,52), (218,125,34), (230,218,41), (40,198,65), (45,147,221), (123,83,173), (27,28,51)]
-  
-# Run until the user asks to quit
-done = False
-while not done:
-    # Did the user click the window close button?
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
 
-    # Fill the background with white
-    surface.fill(colors[-1])
 
-    columns = 16
-    rows = 16
-    xOffset = 32
-    yOffset = 32
-    xGap = 8
-    yGap = 8
-    width = 16
-    height = 16
-    scaling = math.sin(time/30)*3
-    for j in range(columns):
-        for k in range(rows):
-            tX = xOffset + j * (width + xGap)
-            tY = yOffset + k * (height + yGap)
-            #pygame.draw.rect(surface, c, pygame.Rect((tX, tY), (width, height)))
-            c = (j+k+(time//30))%8
-            rectrot(surface, colors[c], pygame.Rect((tX, tY), (width-scaling, height-scaling)), 0, 1, time/3)
 
-    pygame.display.flip()
-    time += 1
-
-# Done! Time to quit.
-pygame.quit()
+wiggleface()
