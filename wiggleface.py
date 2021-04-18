0710377f4d324aca190fd2739aa06d6218def248
Austin Henley <azh321@gmail.com>
2021-04-18 12:56:44 -0400

    Can turn off wiggling.

--- wiggleface.py
+++ wiggleface.py
@@ -9,9 +9,17 @@ class wiggleface:
         self.time = 1000
         # Color palette: https://lospec.com/palette-list/endesga-8
         self.colors = [(253,253,248), (211,39,52), (218,125,34), (230,218,41), (40,198,65), (45,147,221), (123,83,173), (27,28,51)]
-        self.loop()
+        self.columns = 16
+        self.rows = 16
+        self.xOffset = 32
+        self.yOffset = 32
+        self.xGap = 8
+        self.yGap = 8
+        self.width = 16
+        self.height = 16
+        self.grid = [[(0,True)]*self.rows for z in range(self.columns)]
 
-    def loop(self):
+    def start(self, update):
         # Run until the user asks to quit
         done = False
         while not done:
@@ -23,21 +31,19 @@ class wiggleface:
             # Fill the background with white
             self.surface.fill(self.colors[-1])
 
-            columns = 16
-            rows = 16
-            xOffset = 32
-            yOffset = 32
-            xGap = 8
-            yGap = 8
-            width = 16
-            height = 16
-            scaling = math.sin(self.time/30)*3
-            for j in range(columns):
-                for k in range(rows):
-                    tX = xOffset + j * (width + xGap)
-                    tY = yOffset + k * (height + yGap)
-                    c = (j+k+(self.time//30))%8
-                    rectrot(self.surface, self.colors[c], pygame.Rect((tX, tY), (width-scaling, height-scaling)), 0, 1, self.time/3)
+            update()
+
+            scaling = math.sin(self.time/30)*5
+            for j in range(self.columns):
+                for k in range(self.rows):
+                    tX = self.xOffset + j * (self.width + self.xGap)
+                    tY = self.yOffset + k * (self.height + self.yGap)
+                    #c = (j+k+(self.time//30))%8
+                    c = self.grid[j][k][0]
+                    if self.grid[j][k][1]:
+                        rectrot(self.surface, self.colors[c], pygame.Rect((tX, tY), (self.width-scaling, self.height-scaling)), 0, 1, self.time/3)
+                    else:
+                        rectrot(self.surface, self.colors[c], pygame.Rect((tX, tY), (self.width, self.height)), 0, 1, 0)
 
             pygame.display.flip()
             self.time += 1
@@ -57,7 +63,3 @@ def rectrot( surface, color, pos, fill, border_radius, angle ):
     pygame.draw.rect(s, color,(0,0,pos[2],pos[3]),fill, border_radius=border_radius)
     s = pygame.transform.rotate(s,angle)
     surface.blit( s, (pos[0],pos[1]) )
-
-
-
-wiggleface()
