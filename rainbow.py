932ec74498c5357deb79c9c86535b4439f1d8adf
Austin Henley <azh321@gmail.com>
2021-04-18 16:37:19 -0400

    Minor.

---
+++ rainbow.py
@@ -0,0 +1,10 @@
+from wiggleface import *
+
+w = wiggleface()
+w.text = "Welcome to Wiggleface."
+def update():
+    for j in range(w.columns):
+        for k in range(w.rows):
+            m = False if j == 0 or j == w.columns-1 or k == 0 or k == w.rows-1 else True
+            w.grid[j][k] = ((j+k+(w.time//30))%8, m)
+w.start(update)
